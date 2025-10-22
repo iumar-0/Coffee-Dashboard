b3a681324892798c3e72593cca6a35812a83ce72
Muhammad Umar <mianmumar615@gmail.com>
2025-10-23 03:26:33 +0500

    Updated project: major changes and improvements

--- script/loginGSAP.js
+++ script/loginGSAP.js
@@ -1,18 +1,51 @@
 export function loginPageAnimation() {
-    gsap.from(".img-beans ", {
+
+    const timeline = gsap.timeline();
+
+    timeline.from(".id-coffe-land", {
+        y: -100,
+        duration: 2,
+        opacity: 0,
+        ease: "expo.out",
+        onComplete: () => {
+            console.log("Cup animation complted");
+        }
+    })
+    timeline.from(".coffee-land-heading ", {
+        y: -100,
+        duration: 2,
+        opacity: 0,
+        ease: "expo.out",
+        onComplete: () => {
+            console.log("Cup animation complted");
+        }
+    })
+    timeline.from(".coffe-cup-image", {
+        y: -250,
+        duration: 2,
+        opacity: 0,
+        ease: "back.out(1.7)",
+        onComplete: () => {
+            console.log("Cup animation complted");
+        }
+    })
+
+    timeline.from(".img-beans ", {
         x: -200,
         duration: 3,
-        opacity: 1,
+        opacity: 0,
+        ease: "back.out(1.7)",
         onComplete: () => {
             console.log("finshed first");
         }
-    });
-    gsap.from(".img-beans-right", {
+    }, 2);
+    timeline.from(".img-beans-right", {
         x: 200,
+        ease: "back.out(1.7)",
         duration: 3,
-        opacity: 1,
+        opacity: 0,
         onComplete: () => {
             console.log("finshed second")
         }
-    })
+    }, 2);
 }
