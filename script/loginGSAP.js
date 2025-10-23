060b994fda6686ddf6a3fd54968a540cedf2b2d7
Muhammad Umar <mianmumar615@gmail.com>
2025-10-23 22:21:37 +0500

    Updated project

--- script/loginGSAP.js
+++ script/loginGSAP.js
@@ -4,7 +4,7 @@ export function loginPageAnimation() {
 
     timeline.from(".coffee-land-heading ", {
         y: -100,
-        duration: 2.5,
+        duration: 1.5,
         opacity: 0,
         ease: "expo.out",
         onComplete: () => {
@@ -15,7 +15,7 @@ export function loginPageAnimation() {
         y: -100,
         duration: 0.5,
         opacity: 0,
-        ease: "expo.out",
+        ease: "back.out(1.7)",
         onComplete: () => {
             console.log("Cup animation complted");
         }
@@ -38,8 +38,7 @@ export function loginPageAnimation() {
         onComplete: () => {
             console.log("finshed first");
         }
-    }, 2.5);
-    timeline.from(".img-beans-right", {
+    }, 2.5).from(".img-beans-right", {
         x: 200,
         ease: "back.out(1.7)",
         duration: 3,
@@ -48,4 +47,20 @@ export function loginPageAnimation() {
             console.log("finshed second")
         }
     }, 2.5);
+
+    // Repeated Animation of Beans
+
+    // timeline.to(".img-beans", {
+    //     x:"1px",              // move 100px to the right
+    //     duration: 1,         // 1 second forward
+    //     yoyo: true,          // go back to start automatically
+    //     repeat: -1,          // infinite loop
+    //     ease: "power1.inOut" // smooth easing both ways
+    // },4).to(".img-beans-right", {
+    //     x: "1px",              // move 100px to the right
+    //     duration: 1,         // 1 second forward
+    //     yoyo: true,          // go back to start automatically
+    //     repeat: -1,          // infinite loop
+    //     ease: "power1.inOut" // smooth easing both ways
+    // },4)
 };
--- script/script.js
+++ script/script.js
@@ -1,5 +1,15 @@
 import { loginPageAnimation } from "./loginGSAP.js";
 
+// Body control
+window.addEventListener("load", () => {
+    gsap.from("body", {
+        opacity: 0,
+        duration: 4,
+        ease: "back",
+        backgroundColor: "black",
+    })
+})
+// Loading Animation
 window.addEventListener("load", () => {
     document.body.querySelector(".loader-class").style.display = "none";
     document.body.querySelector(".overall").style.display = "block";
